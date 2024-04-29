51475ab2610e406dc96a882eed6de4521354f150
Pedro <pedrohsbelfort@gmail.com>
2024-04-29 14:49:13 -0300

    ident modal code

--- src/components/ModalContent.vue
+++ src/components/ModalContent.vue
@@ -8,130 +8,143 @@ defineProps({
       default: () => []
     }
 })
+
 defineEmits("close")
 </script>
-    <template>        
-        <div>                
-            <base-button @click="$emit('close')" > 
-            <CloseCircleIcon class="secondary" /> 
-            </base-button>
-            <div v-if="items.BMG ==null && items.PAN ==null && items.OLE ==null" class="secondary">
-              <div class="loader"></div>
-            </div>
-  <div v-else class="secondary">
-    <div v-if="items.BMG !=null" class="secondary">
-    <div>
-    <img alt="Vue logo" src="../assets/banco_bmg.png" width="30" height="30" />
-    Simulação BMG
-    </div>
-  <table border="1" id="customers">
-    <thead>
-      <tr>
-        <th>Parcelas</th>
-        <th>Taxa (%)</th>
-        <th>Valor da Parcela (R$)</th>
-        <th>Convênio</th>
-      </tr>
-    </thead>
-    <tbody>
-      <!-- Loop through each loan object in the BMG array -->
-      <tr v-for="(loan, index) in items.BMG" :key="index">
-        <td>{{ loan.parcelas }}</td>
-        <td>{{ loan.taxa }}</td>
-        <td>{{ loan.valor_parcela }}</td>
-        <td>{{ loan.convenio }}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-<div v-else class="secondary">
-      <p>Não foi possível encontrar simulações no banco BMG.</p>
-    </div>
+
+<template>        
+  <div>                
+    <base-button @click="$emit('close')" > 
+      <CloseCircleIcon class="secondary" /> 
+    </base-button>
     
-
-<div v-if="items.PAN != null" class="secondary">
-  <div>
-    <img alt="Vue logo" src="../assets/banco_pan.png" width="30" height="30" />
-    Simulação PAN
-  </div>
-  <table border="1" id="customers">
-    <thead>
-      <tr>
-        <th>Parcelas</th>
-        <th>Taxa (%)</th>
-        <th>Valor da Parcela (R$)</th>
-        <th>Convênio</th>
-      </tr>
-    </thead>
-    <tbody>
-      <!-- Loop through each loan object in the BMG array -->
-      <tr v-for="(loan, index) in items.PAN" :key="index">
-        <td>{{ loan.parcelas }}</td>
-        <td>{{ loan.taxa }}</td>
-        <td>{{ loan.valor_parcela }}</td>
-        <td>{{ loan.convenio }}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-<div v-else class="secondary">
-      <p>Não foi possível encontrar simulações no banco PAN.</p>
+    <div v-if="items.BMG ==null && items.PAN ==null && items.OLE ==null" class="secondary">
+      <p>Aguarde!</p>
+      <p>Estamos buscando as informações da simulação!</p>
+      <br/>
+      <div class="loader"></div>
     </div>
+    <div v-else class="secondary">
 
-<div v-if="items.OLE != null" class="secondary">
-  <div>
-    <img alt="Vue logo" src="../assets/banco_ole.png" width="30" height="30" />
-    Simulação OLE
-  </div>
-  <table border="1" id="customers">
-    <thead>
-      <tr>
-        <th>Parcelas</th>
-        <th>Taxa (%)</th>
-        <th>Valor da Parcela (R$)</th>
-        <th>Convênio</th>
-      </tr>
-    </thead>
-    <tbody>
-      <!-- Loop through each loan object in the BMG array -->
-      <tr v-for="(loan, index) in items.OLE" :key="index">
-        <td>{{ loan.parcelas }}</td>
-        <td>{{ loan.taxa }}</td>
-        <td>{{ loan.valor_parcela }}</td>
-        <td>{{ loan.convenio }}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-<div v-else class="secondary">
-      <p>Não foi possível encontrar simulações no banco OLE.</p>
+      <!--BMG Simulation-->
+      <div v-if="items.BMG !=null" class="secondary">
+        <div>
+          <img alt="Vue logo" src="../assets/banco_bmg.png" width="30" height="30" />
+          Simulação BMG
+        </div>
+        <table border="1" id="simulations">
+          <thead>
+            <tr>
+              <th>Parcelas</th>
+              <th>Taxa (%)</th>
+              <th>Valor da Parcela (R$)</th>
+              <th>Convênio</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- Loop through each loan object in the BMG array -->
+            <tr v-for="(loan, index) in items.BMG" :key="index">
+              <td>{{ loan.parcelas }}</td>
+              <td>{{ loan.taxa }}</td>
+              <td>{{ loan.valor_parcela }}</td>
+              <td>{{ loan.convenio }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else class="secondary">
+        <p>Não foi possível encontrar simulações no banco BMG.</p>
+      </div>
+
+      <!--PAN Simulation--> 
+      <div v-if="items.PAN != null" class="secondary">
+        <div>
+          <img alt="Vue logo" src="../assets/banco_pan.png" width="30" height="30" />
+          Simulação PAN
+        </div>
+        <table border="1" id="simulations">
+          <thead>
+            <tr>
+              <th>Parcelas</th>
+              <th>Taxa (%)</th>
+              <th>Valor da Parcela (R$)</th>
+              <th>Convênio</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- Loop through each loan object in the BMG array -->
+            <tr v-for="(loan, index) in items.PAN" :key="index">
+              <td>{{ loan.parcelas }}</td>
+              <td>{{ loan.taxa }}</td>
+              <td>{{ loan.valor_parcela }}</td>
+              <td>{{ loan.convenio }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else class="secondary">
+        <p>Não foi possível encontrar simulações no banco PAN.</p>
+      </div>
+
+       <!--OLE Simulation--> 
+      <div v-if="items.OLE != null" class="secondary">
+        <div>
+          <img alt="Vue logo" src="../assets/banco_ole.png" width="30" height="30" />
+          Simulação OLE
+        </div>
+        <table border="1" id="simulations">
+          <thead>
+            <tr>
+              <th>Parcelas</th>
+              <th>Taxa (%)</th>
+              <th>Valor da Parcela (R$)</th>
+              <th>Convênio</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- Loop through each loan object in the BMG array -->
+            <tr v-for="(loan, index) in items.OLE" :key="index">
+              <td>{{ loan.parcelas }}</td>
+              <td>{{ loan.taxa }}</td>
+              <td>{{ loan.valor_parcela }}</td>
+              <td>{{ loan.convenio }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else class="secondary">
+        <p>Não foi possível encontrar simulações no banco OLE.</p>
+      </div>
     </div>
-</div>
-</div>
+  </div>
 </template>
 
 <style> 
-#customers {
+#simulations {
   font-family: Arial, Helvetica, sans-serif;
   border-collapse: collapse;
   width: 100%;
 }
 
-#customers td, #customers th {
+#simulations td, #simulations th {
   border: 1px solid #ddd;
   padding: 8px;
 }
 
-#customers tr:nth-child(even){background-color: #f69907;}
+#simulations tr:nth-child(even){background-color: #f69907;}
 
-#customers tr:hover {background-color: #ac6903;}
+#simulations tr:hover {background-color: #ac6903;}
 
-#customers th {
+#simulations th {
   padding-top: 12px;
   padding-bottom: 12px;
   text-align: left;
   background-color: #6ebc6f;
   color: #fdfdc8;
+}
+
+.loader{
+  margin: auto;
 }
 
 </style>
